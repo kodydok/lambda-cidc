048252e93ae06be41bbd5c6bf10f5c1a1f44ee0f
Kody Ma <bykodyma@gmail.com>
2025-02-28 11:35:44 -0500

    more fixes...

--- lambda/lambda_function.py
+++ lambda/lambda_function.py
@@ -3,5 +3,5 @@ import json
 def lambda_handler(event, context):
     return {
         'statusCode': 200,
-        'body': json.dumps('Hello, updates to lambda from vscode')
+        'body': json.dumps('Hello, more updates to lambda from vscode')
     }
